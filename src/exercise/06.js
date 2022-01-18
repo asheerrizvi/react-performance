4c3fd77f82a24a90f147a546d09a34298b3666e2
Asheer Rizvi <asheer.vil@gmail.com>
2022-01-18 15:58:07 +0530

    Exercise 06: Extra Credit 2.

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -110,11 +110,10 @@ function Grid() {
 }
 Grid = React.memo(Grid)
 
-function Cell({row, column}) {
-  const state = useAppState()
-  const cell = state.grid[row][column]
+function CellImpl({cell, row, column}) {
   const dispatch = useAppDispatch()
   const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
+
   return (
     <button
       className="cell"
@@ -127,6 +126,13 @@ function Cell({row, column}) {
       {Math.floor(cell)}
     </button>
   )
+}
+CellImpl = React.memo(CellImpl)
+
+function Cell({row, column}) {
+  const state = useAppState()
+  const cell = state.grid[row][column]
+  return <CellImpl cell={cell} row={row} column={column}></CellImpl>
 }
 Cell = React.memo(Cell)
 
