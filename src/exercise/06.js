30d4c18d30176e18f7a71811466b35e0a6995bdf
Asheer Rizvi <asheer.vil@gmail.com>
2022-01-20 17:42:20 +0530

    Exercise 06: Extra Credit 3.

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -110,7 +110,17 @@ function Grid() {
 }
 Grid = React.memo(Grid)
 
-function CellImpl({cell, row, column}) {
+function withStateSlice(Comp, slice) {
+  const MemoComp = React.memo(Comp)
+  function Wrapper(props, ref) {
+    const state = useAppState()
+    return <MemoComp ref={ref} state={slice(state, props)} {...props} />
+  }
+  Wrapper.displayName = `withStateSlice(${Comp.displayName || Comp.name})`
+  return React.memo(React.forwardRef(Wrapper))
+}
+
+function Cell({state: cell, row, column}) {
   const dispatch = useAppDispatch()
   const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
 
@@ -127,14 +137,7 @@ function CellImpl({cell, row, column}) {
     </button>
   )
 }
-CellImpl = React.memo(CellImpl)
-
-function Cell({row, column}) {
-  const state = useAppState()
-  const cell = state.grid[row][column]
-  return <CellImpl cell={cell} row={row} column={column}></CellImpl>
-}
-Cell = React.memo(Cell)
+Cell = withStateSlice(Cell, (state, {row, column}) => state.grid[row][column])
 
 function DogNameInput() {
   const [state, dispatch] = useDogState()
